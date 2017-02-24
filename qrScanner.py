1dc022274e1810ad55551621c0771ee1335e2fdb
Dhruv Samdani <dhruvsamdani@gmail.com>
2017-02-23 19:58:58 -0500

    Scanner Updates with CSV

--- qrScanner.py
+++ qrScanner.py
@@ -2,12 +2,17 @@ import pygame
 import pygame.camera
 from pygame.locals import *
 import qrtools
+import csv
 
 DEVICE = '/dev/video0'
 SIZE = (640, 480)
 QR_FILE = 'qrCode.png'
 TEAM_NUMBER = input("What team number did you scout? ")
-FILE = 'scoutingFile'
+#FILE = 'scoutingFile'
+WORDS_PER_CHUNK = 3
+SETUP_LIST = 'setupList.csv'
+EVENT_LIST = 'eventList.csv'
+inputString = ""
 
 pygame.camera.init()
 
@@ -32,10 +37,25 @@ while capture:
             qr = qrtools.QR()
             qr.decode(QR_FILE)
             print "Here is the QR Code Data: "
-            print qr.data
-            print "Click the 'X' to exit"
-            f = open(FILE, 'a')
-            f.write('Team Number ' + str(TEAM_NUMBER) + ': ' + qr.data + '\n')
-
+            if(qr.data != "NULL"):
+                inputString = qr.data
+                iAr = inputString.strip().split(",")
+                chunks = lambda iAr, n=WORDS_PER_CHUNK: [iAr[i:i+n] for i in range(0, len(iAr), n)]
+                with open(SETUP_LIST, 'a+') as csvfile:
+                    csvWrite = csv.writer(csvfile, dialect='excel', delimiter=',')
+                    print chunks(iAr)
+                    print len(iAr)
+                    csvWrite.writerow(iAr[:8] + iAr[len(iAr)-6:])
+                with open(EVENT_LIST, 'a+') as csvfile:
+                    csvWrite = csv.writer(csvfile, dialect='excel', delimiter=',')
+                    setupArr = [iAr[2],iAr[1],iAr[5]]
+                    del iAr[:8]
+                    del iAr[len(iAr)-6:]
+                    print iAr
+                    csvWrite.writerow(setupArr + iAr)
+                    print "Click the 'X' to exit"
+            #f = open(FILE, 'a')
+            #f.write('Team Number ' + str(TEAM_NUMBER) + ': ' + qr.data + '\n')
+            
 camera.stop()
 pygame.quit()
